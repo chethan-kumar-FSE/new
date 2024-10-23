144ae00b2550a1cf57ee01a63a6cfd8d2db32297
chethankumar123 <46744971+chethankumar123@users.noreply.github.com>
2024-10-23 19:00:01 +0530

    slug-changed

--- app/(content)/[[...slug]]/page.js
+++ app/(content)/[[...slug]]/page.js
@@ -8,6 +8,13 @@ import { feedsServices } from '@/services/feedsService';
 // Function to generate metadata based on the slug
 export const generateMetadata = async ({ params }) => {
   const slug = params?.slug || [];
+  let lang = false;
+
+  if (slug[0]?.length === 2) {
+    lang = true;
+    // Call the API to set the language cookie
+    //return <Feeds lang={slug[0]} />;
+  }
 
   let title;
   let description;
@@ -17,58 +24,130 @@ export const generateMetadata = async ({ params }) => {
   let keywords;
   let ogUrl;
 
-  const response = await feedsServices.getFeedsSeo({
-    requestBody: {
-      lang: 'en',
-      type: 'home',
-      item: 'home',
-    },
-  });
+  if (!lang) {
+    if (slug.length === 0 || (slug.length === 1 && !slug[0])) {
+      const response = await feedsServices.getFeedsSeo({
+        requestBody: {
+          lang: 'en',
+          type: 'home',
+          item: 'home',
+        },
+      });
+      const { meta_title, meta_description, meta_keywords } = response.data;
 
-  const { meta_title, meta_description, meta_keywords } = response.data;
+      (title = meta_title), (description = meta_description);
+      keywords = meta_keywords;
+      imageUrl =
+        'https://www.hitzfeed.com/trends/media/images/hitzfeed-og-image.jpg';
+      ogUrl = siteUrl;
+    }
+    if (slug.length === 1 && slug[0].includes('-p')) {
+      const postId = slug[0].match(/-p(\d+)/)?.[1];
+      const response = await feedsServices.getFeedById({
+        requestBody: {
+          story_id: postId,
+        },
+      });
+      const { news_title, id, url_string, share_image_link } = response.data[0];
+      category = 'Meme';
 
-  (title = meta_title), (description = meta_description);
-  keywords = meta_keywords;
-  imageUrl =
-    'https://www.hitzfeed.com/trends/media/images/hitzfeed-og-image.jpg';
-  ogUrl = siteUrl;
+      title = `${news_title} ${category} Quote Card - Hitzfeed`;
+      (description = `${news_title} ${category} Quotes on Cards: Check the ${news_title} ${category} quotes in the form of flashcards at Hitzfeed.`),
+        (ogUrl = `${siteUrl}/${url_string}-p${id}/`);
+      (keywords = `${title} ${category} quote on card, ${title} ${category} quote card, ${title}, ${category} quote cards, trending quote cards, latest ${category} quote cards, trending ${category} quote cards`),
+        (imageUrl = `https://imagesvs.oneindia.com/webp/trends${share_image_link}`);
+    }
+    if (slug.length === 1 && slug[0].includes('-c')) {
+      const categoryName = slug[0].split('-c')[0];
+      console.log('executing from inside');
 
-  // Logic to customize metadata based on the slug
-  if (slug.length === 1 && slug[0].includes('-p')) {
-    const postId = slug[0].match(/-p(\d+)/)?.[1];
-    const response = await feedsServices.getFeedById({
-      requestBody: {
-        story_id: postId,
-      },
-    });
-    const { news_title, id, url_string, share_image_link } = response.data[0];
-    category = 'Meme';
+      const response = await feedsServices.getCategorySeo({
+        requestBody: {
+          lang: 'en',
+          type: 'category',
+          item: categoryName,
+        },
+      });
+      const { id, meta_title, meta_description, meta_keywords } = response.data;
+      console.log('response', meta_title);
+      title = `${meta_title} Category - Hitzfeed`;
+      description = meta_description;
+      keywords = meta_keywords;
+      ogUrl = `${siteUrl}/${categoryName}-c${id}/`;
 
-    title = `${news_title} ${category} Quote Card - Hitzfeed`;
-    (description = `${news_title} ${category} Quotes on Cards: Check the ${news_title} ${category} quotes in the form of flashcards at Hitzfeed.`),
-      (ogUrl = `${siteUrl}/${url_string}-p${id}/`);
-    (keywords = `${title} ${category} quote on card, ${title} ${category} quote card, ${title}, ${category} quote cards, trending quote cards, latest ${category} quote cards, trending ${category} quote cards`),
-      (imageUrl = `https://imagesvs.oneindia.com/webp/trends${share_image_link}`);
-  } else if (slug.length === 1 && slug[0].includes('-c')) {
-    const categoryName = slug[0].split('-c')[0];
-
-    const response = await feedsServices.getCategorySeo({
-      requestBody: {
-        lang: 'en',
-        type: 'category',
-        item: categoryName,
-      },
-    });
-    const { id, meta_title, meta_description, meta_keywords } = response.data;
-    title = `${meta_title} Category - Hitzfeed`;
-    description = meta_description;
-    keywords = meta_keywords;
-    ogUrl = `${siteUrl}/${categoryName}-c${id}/`;
-
-    imageUrl = `https://www.hitzfeed.com/trends/media/images/category/250x250/${categoryName}_1.jpg`;
-    // Example for category-specific image
+      imageUrl = `https://www.hitzfeed.com/trends/media/images/category/250x250/${categoryName}_1.jpg`;
+      console.log(
+        'exuecint g-------------------------->>>>>>>>>>>>>>',
+        title,
+        imageUrl
+      );
+      // Example for category-specific image
+    }
   }
 
+  if (lang) {
+    if (slug.length === 1 || (slug.length === 1 && !slug[0])) {
+      const response = await feedsServices.getFeedsSeo({
+        requestBody: {
+          lang: 'en',
+          type: 'home',
+          item: 'home',
+        },
+      });
+      const { meta_title, meta_description, meta_keywords } = response.data;
+
+      (title = meta_title), (description = meta_description);
+      keywords = meta_keywords;
+      imageUrl =
+        'https://www.hitzfeed.com/trends/media/images/hitzfeed-og-image.jpg';
+      ogUrl = siteUrl;
+    }
+    if (slug.length === 1 && slug[0].includes('-p')) {
+      const postId = slug[0].match(/-p(\d+)/)?.[1];
+      const response = await feedsServices.getFeedById({
+        requestBody: {
+          story_id: postId,
+        },
+      });
+      const { news_title, id, url_string, share_image_link } = response.data[0];
+      category = 'Meme';
+
+      title = `${news_title} ${category} Quote Card - Hitzfeed`;
+      (description = `${news_title} ${category} Quotes on Cards: Check the ${news_title} ${category} quotes in the form of flashcards at Hitzfeed.`),
+        (ogUrl = `${siteUrl}/${url_string}-p${id}/`);
+      (keywords = `${title} ${category} quote on card, ${title} ${category} quote card, ${title}, ${category} quote cards, trending quote cards, latest ${category} quote cards, trending ${category} quote cards`),
+        (imageUrl = `https://imagesvs.oneindia.com/webp/trends${share_image_link}`);
+    }
+    if (slug.length === 2 && slug[1].includes('-c')) {
+      const categoryName = slug[1].split('-c')[0];
+      console.log('executing from inside');
+
+      const response = await feedsServices.getCategorySeo({
+        requestBody: {
+          lang: 'en',
+          type: 'category',
+          item: categoryName,
+        },
+      });
+      const { id, meta_title, meta_description, meta_keywords } = response.data;
+      console.log('response', meta_title);
+      title = `${meta_title} Category - Hitzfeed`;
+      description = meta_description;
+      keywords = meta_keywords;
+      ogUrl = `${siteUrl}/${categoryName}-c${id}/`;
+
+      imageUrl = `https://www.hitzfeed.com/trends/media/images/category/250x250/${categoryName}_1.jpg`;
+      console.log(
+        'exuecint g-------------------------->>>>>>>>>>>>>>',
+        title,
+        imageUrl
+      );
+      // Example for category-specific image
+    }
+  }
+  // Logic to customize metadata based on the slug
+
+  console.log('title', title, description, imageUrl);
   return {
     title: title,
     description: description,
