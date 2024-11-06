03bb79a0b2f595de0e0734376ec1df2e2bc91bea
chethankumar123 <46744971+chethankumar123@users.noreply.github.com>
2024-11-06 12:01:24 +0530

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -15,129 +15,132 @@ const pwaConfig = withPWA({
   fallbacks: '',
   workboxOptions: {
     runtimeCaching: [
-      {
-        urlPattern: /^https:\/\/fonts\.(?:gstatic)\.com\/.*/i,
-        handler: 'CacheFirst',
-        options: {
-          cacheName: 'google-fonts-webfonts-cached',
-          expiration: {
-            maxEntries: 4,
-            maxAgeSeconds: 365 * 24 * 60 * 60, // 365 days
-          },
-        },
+  {
+    urlPattern: new RegExp('^https://fonts\\.gstatic\\.com/.*', 'i'),
+    handler: 'CacheFirst',
+    options: {
+      cacheName: 'google-fonts-webfonts-cached',
+      expiration: {
+        maxEntries: 4,
+        maxAgeSeconds: 365 * 24 * 60 * 60, // 365 days
       },
-      {
-        urlPattern: /^https:\/\/fonts\.(?:googleapis)\.com\/.*/i,
-        handler: 'StaleWhileRevalidate',
-        options: {
-          cacheName: 'google-fonts-stylesheets-cached',
-          expiration: {
-            maxEntries: 4,
-            maxAgeSeconds: 7 * 24 * 60 * 60, // 7 days
-          },
-        },
+    },
+  },
+  {
+    urlPattern: new RegExp('^https://fonts\\.googleapis\\.com/.*', 'i'),
+    handler: 'StaleWhileRevalidate',
+    options: {
+      cacheName: 'google-fonts-stylesheets-cached',
+      expiration: {
+        maxEntries: 4,
+        maxAgeSeconds: 7 * 24 * 60 * 60, // 7 days
       },
-      {
-        urlPattern: /\.(?:eot|otf|ttc|ttf|woff|woff2|font.css)$/i,
-        handler: 'StaleWhileRevalidate',
-        options: {
-          cacheName: 'static-font-assets-cached',
-          expiration: {
-            maxEntries: 4,
-            maxAgeSeconds: 7 * 24 * 60 * 60, // 7 days
-          },
-        },
+    },
+  },
+  {
+    urlPattern: new RegExp('/\\.(?:eot|otf|ttc|ttf|woff|woff2|font\\.css)$/i'),
+    handler: 'StaleWhileRevalidate',
+    options: {
+      cacheName: 'static-font-assets-cached',
+      expiration: {
+        maxEntries: 4,
+        maxAgeSeconds: 7 * 24 * 60 * 60, // 7 days
       },
-      {
-        urlPattern: /\.(?:jpg|jpeg|gif|png|svg|ico|webp)$/i,
-        handler: 'StaleWhileRevalidate',
-        options: {
-          cacheName: 'static-image-assets',
-          expiration: {
-            maxEntries: 64,
-            maxAgeSeconds: 24 * 60 * 60, // 24 hours
-          },
-        },
+    },
+  },
+  {
+    urlPattern: new RegExp('/\\.(?:jpg|jpeg|gif|png|svg|ico|webp)$/i'),
+    handler: 'StaleWhileRevalidate',
+    options: {
+      cacheName: 'static-image-assets',
+      expiration: {
+        maxEntries: 64,
+        maxAgeSeconds: 24 * 60 * 60, // 24 hours
       },
-      {
-        urlPattern: /\/_next\/image\?url=.+$/i,
-        handler: 'StaleWhileRevalidate',
-        options: {
-          cacheName: 'next-image',
-          expiration: {
-            maxEntries: 64,
-            maxAgeSeconds: 24 * 60 * 60, // 24 hours
-          },
-        },
+    },
+  },
+  {
+    urlPattern: new RegExp('/_next/image\\?url=.+$', 'i'),
+    handler: 'StaleWhileRevalidate',
+    options: {
+      cacheName: 'next-image',
+      expiration: {
+        maxEntries: 64,
+        maxAgeSeconds: 24 * 60 * 60, // 24 hours
       },
-      {
-        urlPattern: /\.(?:mp3|wav|ogg)$/i,
-        handler: 'CacheFirst',
-        options: {
-          rangeRequests: true,
-          cacheName: 'static-audio-assets',
-          expiration: {
-            maxEntries: 32,
-            maxAgeSeconds: 24 * 60 * 60, // 24 hours
-          },
-        },
+    },
+  },
+  {
+    urlPattern: new RegExp('/\\.(?:mp3|wav|ogg)$/i'),
+    handler: 'CacheFirst',
+    options: {
+      rangeRequests: true,
+      cacheName: 'static-audio-assets',
+      expiration: {
+        maxEntries: 32,
+        maxAgeSeconds: 24 * 60 * 60, // 24 hours
       },
-      {
-        urlPattern: /\.(?:mp4)$/i,
-        handler: 'CacheFirst',
-        options: {
-          rangeRequests: true,
-          cacheName: 'static-video-assets',
-          expiration: {
-            maxEntries: 32,
-            maxAgeSeconds: 24 * 60 * 60, // 24 hours
-          },
-        },
+    },
+  },
+  {
+    urlPattern: new RegExp('/\\.(?:mp4)$/i'),
+    handler: 'CacheFirst',
+    options: {
+      rangeRequests: true,
+      cacheName: 'static-video-assets',
+      expiration: {
+        maxEntries: 32,
+        maxAgeSeconds: 24 * 60 * 60, // 24 hours
       },
-      {
-        urlPattern: /\.(?:js)$/i,
-        handler: 'StaleWhileRevalidate',
-        options: {
-          cacheName: 'static-js-assets',
-          expiration: {
-            maxEntries: 32,
-            maxAgeSeconds: 24 * 60 * 60, // 24 hours
-          },
-        },
+    },
+  },
+  {
+    urlPattern: new RegExp('/\\.(?:js)$/i'),
+    handler: 'StaleWhileRevalidate',
+    options: {
+      cacheName: 'static-js-assets',
+      expiration: {
+        maxEntries: 32,
+        maxAgeSeconds: 24 * 60 * 60, // 24 hours
       },
-      {
-        urlPattern: /\.(?:css|less)$/i,
-        handler: 'StaleWhileRevalidate',
-        options: {
-          cacheName: 'static-style-assets',
-          expiration: {
-            maxEntries: 32,
-            maxAgeSeconds: 24 * 60 * 60, // 24 hours
-          },
-        },
+    },
+  },
+  {
+    urlPattern: new RegExp('/\\.(?:css|less)$/i'),
+    handler: 'StaleWhileRevalidate',
+    options: {
+      cacheName: 'static-style-assets',
+      expiration: {
+        maxEntries: 32,
+        maxAgeSeconds: 24 * 60 * 60, // 24 hours
       },
-      {
-        urlPattern: /\/_next\/data\/.+\/.+\.json$/i,
-        handler: 'StaleWhileRevalidate',
-        options: {
-          cacheName: 'next-data',
-          expiration: {
-            maxEntries: 32,
-            maxAgeSeconds: 24 * 60 * 60, // 24 hours
-          },
-        },
+    },
+  },
+  {
+    urlPattern: new RegExp('/_next/data/.+/.+\\.json$', 'i'),
+    handler: 'StaleWhileRevalidate',
+    options: {
+      cacheName: 'next-data',
+      expiration: {
+        maxEntries: 32,
+        maxAgeSeconds: 24 * 60 * 60, // 24 hours
       },
-      {
-        urlPattern: /\.(?:json|xml|csv)$/i,
-        handler: 'NetworkFirst',
-        options: {
-          cacheName: 'static-data-assets',
-          expiration: {
-            maxEntries: 32,
-            maxAgeSeconds: 24 * 60 * 60, // 24 hours
-          },
-        },
+    },
+  },
+  {
+    urlPattern: new RegExp('/\\.(?:json|xml|csv)$/i'),
+    handler: 'NetworkFirst',
+    options: {
+      cacheName: 'static-data-assets',
+      expiration: {
+        maxEntries: 32,
+        maxAgeSeconds: 24 * 60 * 60, // 24 hours
       },
+    },
+  },
+  // Additional URL patterns as needed...
+
+
       {
         urlPattern: ({ url }) => {
           const isSameOrigin = self.origin === url.origin;
