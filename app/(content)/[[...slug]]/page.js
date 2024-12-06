72e3c96c4a86dd56295ec2dfc4238153371ce8a0
chethankumar123 <chethananderson4@gmail.com>
2024-12-06 10:38:28 +0530

    slugedcahnge

--- app/(content)/[[...slug]]/page.js
+++ app/(content)/[[...slug]]/page.js
@@ -1,38 +1,38 @@
 import Feeds from '../(subcontent)/feeds/page';
-import PostById from '../(subcontent)/feeds/[postId]/page';
+import IndiFeeds from '../(subcontent)/feeds/[postId]/page';
 import Category from '../category/page';
 import { notFound } from 'next/navigation';
 import Head from 'next/head';
 import { feedsServices } from '@/services/feedsService';
-import Trending from '../(subcontent)/trending/page';
-import { LANGUAGES } from '@/utils/constant';
-import { ErrorBoundary } from 'react-error-boundary';
-import { Fallback } from '@/components/Fallback';
-import { Suspense } from 'react';
-import Loader from '@/components/Loader';
 
-export const generateMetadata = async ({ index, slug }) => {
+// Function to generate metadata based on the slug
+export const generateMetadata = async ({ params }) => {
+  const slug = params?.slug || [];
+  let lang = false;
+
+  if (slug[0]?.length === 2) {
+    lang = true;
+    // Call the API to set the language cookie
+    //return <Feeds lang={slug[0]} />;
+  }
+
   let title;
   let description;
-  let imageUrl;
+  let imageUrl; // Default image
   const siteUrl = process.env.NEXT_PUBLIC_API_URL;
   let keywords;
   let ogUrl;
 
-  if (!slug) return;
-  //if slug's length equals to index then return FeedsSeo
-  /*   if (slug?.length === index || (slug?.length === 1 && !slug[0])) {
-    try {
-      //fetching feeds seo data
-      const homeSeoData = await feedsServices?.getFeedsSeo({
+  if (!lang) {
+    if (slug.length === 0 || (slug.length === 1 && !slug[0])) {
+      const response = await feedsServices.getFeedsSeo({
         requestBody: {
           lang: 'en',
           type: 'home',
           item: 'home',
         },
       });
-
-      const { meta_title, meta_description, meta_keywords } = homeSeoData;
+      const { meta_title, meta_description, meta_keywords } = response.data;
 
       title = meta_title;
       description = meta_description;
@@ -40,75 +40,124 @@ export const generateMetadata = async ({ index, slug }) => {
       imageUrl =
         'https://www.hitzfeed.com/trends/media/images/hitzfeed-og-image.jpg';
       ogUrl = siteUrl;
-      console.log('metadtanotfrom home');
-      return metaObject({ title, description, ogUrl, imageUrl, keywords });
-    } catch (err) {
-      console.log(err);
     }
-  } */
-
-  //extract post id if reg ex matches the condition
-  let postId = slug[index]?.match(/-p(\d+)/)?.[1];
-  if (postId) {
-    try {
-      const feedById = await feedsServices?.getFeedById({
+    if (slug.length === 1 && slug[0].includes('-p')) {
+      const postId = slug[0].match(/-p(\d+)/)?.[1];
+      const response = await feedsServices.getFeedById({
         requestBody: {
           story_id: postId,
         },
       });
+      const {
+        news_title,
+        id,
+        url_string,
+        share_image_link,
+        item_category: category,
+      } = response.data[0];
 
-      const { news_title, id, url_string, share_image_link, item_category } =
-        feedById[0];
-
-      title = `${news_title} ${item_category} Quote Card - Hitzfeed`;
-      description = `${news_title} ${item_category} Quotes on Cards: Check the ${news_title} ${item_category} quotes in the form of flashcards at Hitzfeed.`;
+      title = `${news_title} ${category} Quote Card - Hitzfeed`;
+      description = `${news_title} ${category} Quotes on Cards: Check the ${news_title} ${category} quotes in the form of flashcards at Hitzfeed.`;
       ogUrl = `${siteUrl}/${url_string}-p${id}/`;
-      keywords = `${title} ${item_category} quote on card, ${title} ${item_category} quote card, ${title}, ${item_category} quote cards, trending quote cards, latest ${item_category} quote cards, trending ${item_category} quote cards`;
+      keywords = `${title} ${category} quote on card, ${title} ${category} quote card, ${title}, ${category} quote cards, trending quote cards, latest ${category} quote cards, trending ${category} quote cards`;
       imageUrl = `https://imagesvs.oneindia.com/webp/trends${share_image_link}`;
-
-      return metaObject({ title, description, ogUrl, keywords, imageUrl });
-    } catch (err) {
-      console.log(err);
     }
-  }
+    if (slug.length === 1 && slug[0].includes('-c')) {
+      const categoryName = slug[0].split('-c')[0];
+      console.log('executing from inside');
 
-  //checking if its a catgory page
-
-  if (slug[index]?.match(/-c\d{1,2}/)) {
-    try {
-      const indexed = slug[index]?.lastIndexOf('-c');
-      const categoryName = slug[index]?.slice(0, indexed);
-
-      const categorySeoData = await feedsServices?.getCategorySeo({
+      const response = await feedsServices.getCategorySeo({
         requestBody: {
           lang: 'en',
           type: 'category',
           item: categoryName,
         },
       });
-
-      const { id, meta_title, meta_description, meta_keywords } =
-        categorySeoData;
-
+      const { id, meta_title, meta_description, meta_keywords } = response.data;
+      console.log('response', meta_title);
       title = `${meta_title} Category - Hitzfeed`;
       description = meta_description;
       keywords = meta_keywords;
       ogUrl = `${siteUrl}/${categoryName}-c${id}/`;
 
       imageUrl = `https://www.hitzfeed.com/trends/media/images/category/250x250/${categoryName}_1.jpg`;
-
-      return metaObject({ title, description, keywords, ogUrl, imageUrl });
-    } catch (err) {
-      console.log(err);
+      console.log(
+        'exuecint g-------------------------->>>>>>>>>>>>>>',
+        title,
+        imageUrl
+      );
+      // Example for category-specific image
     }
   }
-};
 
-// -c is for category
-// -p is for posts
+  if (lang) {
+    if (slug.length === 1 || (slug.length === 1 && !slug[0])) {
+      const response = await feedsServices.getFeedsSeo({
+        requestBody: {
+          lang: 'en',
+          type: 'home',
+          item: 'home',
+        },
+      });
+      const { meta_title, meta_description, meta_keywords } = response.data;
 
-//function to return common SEO data for Feeds, Category , FeedsById
-function metaObject({ title, description, keywords, ogUrl, imageUrl }) {
+      (title = meta_title), (description = meta_description);
+      keywords = meta_keywords;
+      imageUrl =
+        'https://www.hitzfeed.com/trends/media/images/hitzfeed-og-image.jpg';
+      ogUrl = siteUrl;
+    }
+    if (slug.length === 1 && slug[0].includes('-p')) {
+      const postId = slug[0].match(/-p(\d+)/)?.[1];
+      const data = await feedsServices.getFeedById({
+        requestBody: {
+          story_id: postId,
+        },
+      });
+      const {
+        news_title,
+        id,
+        url_string,
+        share_image_link,
+        item_category: category,
+      } = data[0];
+
+      title = `${news_title} ${category} Quote Card - Hitzfeed`;
+      (description = `${news_title} ${category} Quotes on Cards: Check the ${news_title} ${category} quotes in the form of flashcards at Hitzfeed.`),
+        (ogUrl = `${siteUrl}/${url_string}-p${id}/`);
+      (keywords = `${title} ${category} quote on card, ${title} ${category} quote card, ${title}, ${category} quote cards, trending quote cards, latest ${category} quote cards, trending ${category} quote cards`),
+        (imageUrl = `https://imagesvs.oneindia.com/webp/trends${share_image_link}`);
+    }
+    if (slug.length === 2 && slug[1].includes('-c')) {
+      const categoryName = slug[1].split('-c')[0];
+      console.log('executing from inside');
+
+      const response = await feedsServices.getCategorySeo({
+        requestBody: {
+          lang: 'en',
+          type: 'category',
+          item: categoryName,
+        },
+      });
+      const { id, meta_title, meta_description, meta_keywords } = response.data;
+      console.log('response', meta_title);
+      title = `${meta_title} Category - Hitzfeed`;
+      description = meta_description;
+      keywords = meta_keywords;
+      ogUrl = `${siteUrl}/${categoryName}-c${id}/`;
+
+      imageUrl = `https://www.hitzfeed.com/trends/media/images/category/250x250/${categoryName}_1.jpg`;
+      console.log(
+        'exuecint g-------------------------->>>>>>>>>>>>>>',
+        title,
+        imageUrl
+      );
+      // Example for category-specific image
+    }
+  }
+  // Logic to customize metadata based on the slug
+
+  console.log('title', title, description, imageUrl);
   return {
     title: title,
     description: description,
@@ -124,85 +173,155 @@ function metaObject({ title, description, keywords, ogUrl, imageUrl }) {
       canonical: '/',
     },
   };
-}
+};
 
 export default async function Page({ params }) {
   const slug = params?.slug || [];
+  let lang = false;
 
-  //setting flag variable to check if language is present
-  let isLanguagePresent = false;
+  // Generate dynamic metadata based on slug
+  const metadata = await generateMetadata({ params });
 
-  //check if language is present as two charactes in the URL
+  // Check if the first slug segment is a language prefix (2 characters long)
   if (slug[0]?.length === 2) {
-    //if not present in the language list then throw the error
-    if (!LANGUAGES?.includes(slug[0])) {
-      return notFound();
-    }
-
-    isLanguagePresent = true;
-  }
-
-  let componentToRender = null;
-
-  //based on the language presence access the index value from slug
-  let index = isLanguagePresent ? 1 : 0;
-
-  const metadata = await generateMetadata({ index, slug });
-  console.log('metadat', metadata);
-  //check if slug has langauge and trending
-  if (slug?.length === 2 && slug[1] === 'trending') {
-    return (
-      <Suspense fallback={<Loader />}>
-        <ErrorBoundary FallbackComponent={Fallback}>
-          <Trending />;
-        </ErrorBoundary>
-      </Suspense>
-    );
+    lang = true;
+    // Call the API to set the language cookie
+    //return <Feeds lang={slug[0]} />;
   }
 
   // Handle routes based on `lang` and `slug`
-  if (slug?.length === index || (slug?.length === index && !slug[0])) {
-    componentToRender = <Feeds />;
+  if (!lang) {
+    if (slug.length === 0 || (slug.length === 1 && !slug[0])) {
+      return (
+        <>
+          <Head>
+            <title>{metadata.title}</title>
+            <meta name="description" content={metadata.description} />
+            <meta property="og:title" content={metadata.openGraph.title} />
+            <meta
+              property="og:description"
+              content={metadata.openGraph.description}
+            />
+            <meta
+              property="og:image"
+              content={metadata.openGraph.images[0].url}
+            />
+          </Head>
+          <Feeds lang={undefined} />
+        </>
+      );
+    }
+
+    if (slug.length === 1 && slug[0].includes('-p')) {
+      const postId = slug[0].match(/-p(\d+)/)?.[1];
+      return (
+        <>
+          <Head>
+            <title>{metadata.title}</title>
+            <meta name="description" content={metadata.description} />
+            <meta property="og:title" content={metadata.openGraph.title} />
+            <meta
+              property="og:description"
+              content={metadata.openGraph.description}
+            />
+            <meta
+              property="og:image"
+              content={metadata.openGraph.images[0].url}
+            />
+          </Head>
+          <IndiFeeds params={{ postId }} />
+        </>
+      );
+    }
+
+    if (slug.length === 1 && slug[0].includes('-c')) {
+      const categoryId = slug[0].split('-c')[1];
+      return (
+        <>
+          <Head>
+            <title>{metadata.title}</title>
+            <meta name="description" content={metadata.description} />
+            <meta property="og:title" content={metadata.openGraph.title} />
+            <meta
+              property="og:description"
+              content={metadata.openGraph.description}
+            />
+            <meta
+              property="og:image"
+              content={metadata.openGraph.images[0].url}
+            />
+          </Head>
+          <Category params={{ id: categoryId }} />
+        </>
+      );
+    }
   }
 
-  //checking if postId is valid based on the reg exp
-  const postId = slug[index]?.match(/-p(\d+)/)?.[1];
+  if (lang) {
+    if (slug.length === 1 || (slug.length === 1 && !slug[0])) {
+      return (
+        <>
+          <Head>
+            <title>{metadata.title}</title>
+            <meta name="description" content={metadata.description} />
+            <meta property="og:title" content={metadata.openGraph.title} />
+            <meta
+              property="og:description"
+              content={metadata.openGraph.description}
+            />
+            <meta
+              property="og:image"
+              content={metadata.openGraph.images[0].url}
+            />
+          </Head>
+          <Feeds lang={slug[0]} />
+        </>
+      );
+    }
+    if (slug.length === 2 && slug[1].includes('-p')) {
+      const postId = slug[1].match(/-p(\d+)/)?.[1];
+      return (
+        <>
+          <Head>
+            <title>{metadata.title}</title>
+            <meta name="description" content={metadata.description} />
+            <meta property="og:title" content={metadata.openGraph.title} />
+            <meta
+              property="og:description"
+              content={metadata.openGraph.description}
+            />
+            <meta
+              property="og:image"
+              content={metadata.openGraph.images[0].url}
+            />
+          </Head>
+          <IndiFeeds params={{ postId }} lang={slug[0]} />
+        </>
+      );
+    }
 
-  if (postId) {
-    componentToRender = <PostById params={{ postId }} />;
+    if (slug.length === 2 && slug[1].includes('-c')) {
+      const categoryId = slug[1].split('-c')[1];
+      return (
+        <>
+          <Head>
+            <title>{metadata.title}</title>
+            <meta name="description" content={metadata.description} />
+            <meta property="og:title" content={metadata.openGraph.title} />
+            <meta
+              property="og:description"
+              content={metadata.openGraph.description}
+            />
+            <meta
+              property="og:image"
+              content={metadata.openGraph.images[0].url}
+            />
+          </Head>
+          <Category params={{ id: categoryId }} lang={slug[0]} />
+        </>
+      );
+    }
   }
 
-  //extract and check if the categoryName matches the condition
-  if (slug[index]?.match(/-c\d{1,2}/)) {
-    const indexed = slug[index]?.lastIndexOf('-c');
-    const categoryId = slug[index].slice(indexed + 2);
-    componentToRender = <Category params={{ categoryId }} />;
-  }
-
-  //if componentToRender is null then return notfound since no if check was matched
-  if (!componentToRender) {
-    return notFound();
-  }
-  console.log('another meta data', metadata);
-  return (
-    <>
-      {/* if meta data present only returning Head component to appear in the SEO */}
-      {metadata && (
-        <Head>
-          <title>{metadata?.title}</title>
-          <meta name="description" content={metadata?.description} />
-          <meta property="og:title" content={metadata?.openGraph?.title} />
-          <meta
-            property="og:description"
-            content={metadata?.openGraph?.description}
-          />
-          <meta property="og:image" content={metadata?.openGraph?.images[0]} />
-        </Head>
-      )}
-      {console.log('metadata', metadata)}
-      <ErrorBoundary FallbackComponent={Fallback}>
-        {componentToRender}
-      </ErrorBoundary>
-    </>
-  );
+  return notFound();
 }
